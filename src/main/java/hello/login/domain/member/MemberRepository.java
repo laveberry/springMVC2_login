91024c8b7eea733d406e8f3309a50ae4f2f84841
laveberry <laveberry7@gmail.com>
2023-04-17 21:56:55 +0900

    회원가입

---
+++ src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,52 @@
+package hello.login.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+//Member저장소
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); //static 사용
+    private static long sequence = 0L; //static
+
+    public Member save(Member member){
+        member.setId(++sequence);
+        log.info("save: member={}", member);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id){
+        return store.get(id);
+    }
+
+    public Optional<Member> findByLoginId(String loginId){
+        /*
+        List<Member> all = findAll();
+        for(Member m : all){
+            if(m.getLoginId().equals(loginId)){
+                //return m;
+                return Optional.of(m);
+            }
+        }
+        return Optional.empty();
+         */
+
+        return findAll().stream()
+                .filter(m -> m.getLoginId().equals(loginId))
+                .findFirst(); //먼저 나오는값 반환
+    }
+
+    public List<Member> findAll(){
+        //key에 대한 value들이 리스트로 변환
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
