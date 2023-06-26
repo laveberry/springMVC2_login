d0f0dcfa33b19afb316815a6d248958f81f72ffb
laveberry <laveberry7@gmail.com>
2023-06-27 06:33:06 +0900

    로그인 처리하기 - 서블릿HTTP 세션2

--- src/main/java/hello/login/web/SessionConst.java
+++ src/main/java/hello/login/web/SessionConst.java
@@ -3,6 +3,6 @@ package hello.login.web;
 //세션 상수
 //수정하지 않으니 추상클래스 또는 인터페이스로 생성
 public class SessionConst {
-    public static String LOGIN_MEMBER = "loginMember";
+    public static final String LOGIN_MEMBER = "loginMember";
 
 }
